4e8ec7d15c0ee3d94b48ca4e8352a791610123e0
Navarr Barnier <navarr@gtaero.net>
2011-10-30 03:34:47 -0400

    More Settings

--- fancy-settings/manifest.js
+++ fancy-settings/manifest.js
@@ -69,6 +69,12 @@ this.manifest = {
 			"group": chrome.i18n.getMessage("creditsTab_donorsGroup"),
 			"type": "description",
 			"text": chrome.i18n.getMessage("creditsTab_donorsGroup_list")
+		},
+		{
+			"tab": chrome.i18n.getMessage("creditsTab"),
+			"group": chrome.i18n.getMessage("creditsTab_donateGroup"),
+			"type": "description",
+			"text": '<form action="https://www.paypal.com/cgi-bin/webscr" method="post" id="donateButton"><input type="hidden" name="cmd" value="_s-xclick"><input type="hidden" name="hosted_button_id" value="WFAZD6HFNB9MC"><input type="image" src="https://www.paypalobjects.com/' + chrome.i18n.getMessage('@@ui_locale') + '/i/btn/btn_donate_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!"><img alt="" border="0" src="https://www.paypalobjects.com/' + chrome.i18n.getMessage('@@ui_locale') + '/i/scr/pixel.gif" width="1" height="1"></form>'
 		}
 	]
 };
