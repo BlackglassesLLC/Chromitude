54bac6ba0874683a1c5820190aac0ebdcd2b7c0c
Navarr Barnier <me@navarr.me>
2013-05-15 22:01:57 -0400

    Attempt to get rid of the dreaded "Invalid Client ID" Remove conflict with other apps using this OAuth2 library (i.e. Chrome to Mobile)

--- chromitude.js
+++ chromitude.js
@@ -1,3 +1,12 @@
+settings =
+{
+    oauth_domain: '891356443055.apps.googleusercontent.com',
+    consumer_key: '891356443055.apps.googleusercontent.com',
+    consumer_secret: 'iEcwn1/WBJN641EwVnRI4S7I',
+    simple_key: 'AIzaSyCsAmNmY6qz8ibtX31mbPQOsC_AN6g-CxE',
+    app_name: 'Chromitude'
+};
+
 function startProgram() {
 
     var clientSettings = new Store("settings", {
